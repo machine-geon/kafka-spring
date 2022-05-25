733112505c867ee39b8fb0a60c8318d0e516a466
mgkim <mgkim@sparkandassociates.com>
2022-05-25 20:23:02 +0900

    clip4-1

--- src/main/java/com/mingeon/KafkaApplication.java
+++ src/main/java/com/mingeon/KafkaApplication.java
@@ -1,13 +1,12 @@
 package com.mingeon;
 
-import java.nio.charset.StandardCharsets;
-
 import com.mingeon.producer.ClipProducer;
 
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
 
 @SpringBootApplication
 public class KafkaApplication {
@@ -17,13 +16,24 @@ public class KafkaApplication {
     }
 
     @Bean
-    public ApplicationRunner runner(ClipProducer clipProducer) {
+    public ApplicationRunner runner(ClipProducer clipProducer, KafkaMessageListenerContainer<String, String> kafkaMessageListenerContainer) {
         return args -> {
-            clipProducer.async("clip3", "Hello, clip3-async");
-            clipProducer.sync("clip3", "Hello, clip3-sync");
-            clipProducer.routingSend("clip3", "Hello, clip3-routing");
-            clipProducer.routingSendBytes("clip3-bytes", "Hello, clip3-bytes".getBytes(StandardCharsets.UTF_8));
-            clipProducer.replyingSend("clip3-request", "Ping Clip3");
+            clipProducer.async("clip4", "Hello, clip4 Container.");
+            kafkaMessageListenerContainer.start();
+            Thread.sleep(1000L);
+
+            System.out.println("-- pause --");
+            kafkaMessageListenerContainer.pause();
+            Thread.sleep(5000L);
+
+            clipProducer.async("clip4", "Hello, Secondly clip4 Container.");
+
+            System.out.println("-- resume --");
+            kafkaMessageListenerContainer.resume();
+            Thread.sleep(1000L);
+
+            System.out.println("-- stop--");
+            kafkaMessageListenerContainer.stop();
         };
     }
 }
--- src/main/java/com/mingeon/consumer/ClipConsumer.java
+++ src/main/java/com/mingeon/consumer/ClipConsumer.java
@@ -1,26 +1,8 @@
 package com.mingeon.consumer;
 
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClipConsumer {
 
-    @KafkaListener(id = "clip3-id", topics = "clip3")
-    public void listenClip3(String message) {
-        System.out.println(message);
-    }
-
-    @KafkaListener(id = "clip3-bytesid", topics = "clip3-bytes")
-    public void listenClip3Bytes(String message) {
-        System.out.println(message);
-    }
-
-    @KafkaListener(id = "clip3-request-id", topics = "clip3-bytes")
-    @SendTo
-    public String listenClipRequest(String message) {
-        System.out.println(message);
-        return "Pong clip3";
-    }
 }
